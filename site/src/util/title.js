305996357e6087848a4d123ee8409911a0f538fb
Aster <speccer.fim@gmail.com>
2026-02-04 18:32:27 +0200

    fix(header): fixed title animation script

--- site/src/util/title.js
+++ site/src/util/title.js
@@ -1,8 +1,14 @@
 document.querySelectorAll('.enby-text').forEach(el => {
   el.addEventListener('mouseenter', () => {
-    el.classList.remove('is-animating');
-    void el.offsetWidth;                
+    if (el.classList.contains('is-animating')) {
+      return;
+    }
     el.classList.add('is-animating');    
+    setTimeout(() => {
+      el.classList.remove('is-animating');
+      void el.offsetWidth;                
+    }, 3000);
+    
   });
 });
 
