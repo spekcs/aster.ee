ee0bd36d68281314638e4d0e9a39d8ac0ee5399d
Aster <speccer.fim@gmail.com>
2026-02-04 03:05:42 +0200

    feat: added latex support

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -2,9 +2,20 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
+import mdx from '@astrojs/mdx';
+
+import preact from '@astrojs/preact';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
+  },
+
+  integrations: [mdx(), preact()],
+  markdown: {
+    shikiConfig: {
+      theme: 'catppuccin-mocha',
+    }
   }
 });
